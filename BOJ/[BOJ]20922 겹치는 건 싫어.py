7bb89c548d2b0cef7661397df456a756ef372de9
seoljeongwoo <sjw2649@gmail.com>
2021-07-20 21:07:25 +0900

    [BOJ]20922 겹치는 건 싫어.py

---
+++ BOJ/[BOJ]20922 겹치는 건 싫어.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N,K = map(int,input().rstrip('\n').split())
+lst = list(map(int,input().rstrip('\n').split()))
+cnt = [0]*(max(lst)+1)
+
+lp,rp,ret=0,0,0
+while rp < N:
+    print(lp,rp)
+    if cnt[lst[rp]] < K:
+        cnt[lst[rp]] += 1
+        rp +=1
+    else:
+        ret = max(ret, rp-lp)
+        cnt[lst[lp]] -= 1
+        lp +=1
+ret = max(ret, rp-lp)
+print(ret)
