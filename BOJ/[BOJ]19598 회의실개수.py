8b4205ee1a7a2793f6df774b14cea9f0ff6dbc2a
seoljeongwoo <sjw2649@gmail.com>
2021-08-01 17:15:22 +0900

    [BOJ]19598 회의실 개수.py

---
+++ BOJ/[BOJ]19598 회의실개수.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+for _ in range(N):
+    st,en = map(int,input().rstrip('\n').split())
+    lst.append((st, 1))
+    lst.append((en,-1))
+lst.sort()
+print(lst)
+room , ret = 0, 0
+for index, data in enumerate(lst):
+    room += data[1]
+    ret = max(ret,room)
+print(ret)
